d5df8015b92f6b2b1df265077ebc79c73c11f2eb
akht <picopeko@gmail.com>
2023-04-27 19:46:39 +0900

    有給を考慮に入れる

--- scripts/content.js
+++ scripts/content.js
@@ -11,7 +11,10 @@
   const workingDaysElement = document.querySelector('#time-card-accordion-01 > div > div > table > tbody > tr > td:nth-child(2) > span');
   const workingDays = Number(workingDaysElement.textContent.trim());
 
-  const remainingDays = totalDays - workingDays;
+  const paidLeaveDaysElement = document.querySelector('#time-card-accordion-01 > div > div > table > tbody > tr > td:nth-child(5) > span');
+  const paidLeaveDays = Number(paidLeaveDaysElement.textContent.trim());
+
+  const remainingDays = totalDays - workingDays - Math.floor(paidLeaveDays);
   const minutesPerDay = remainingDays > 0 ? remainingMinutes / remainingDays : 0;
   const hourPerDay = Math.ceil(minutesPerDay / 60 * Math.pow(10, 2) ) / Math.pow(10, 2); // 切り上げ
 
